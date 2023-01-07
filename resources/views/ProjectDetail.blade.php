8d8bba95b864195891c86bcf16cfad84246abfee
sagardhakal <sagar.dhakal312@gmail.com>
2023-01-07 13:57:47 +0545

    project detail

--- resources/views/ProjectDetail.blade.php
+++ resources/views/ProjectDetail.blade.php
@@ -1,6 +1,7 @@
 @extends("layout")
 @section("content")
 
+<div>
 <div class="col-xs-12 col-sm-7 col-md-8 col-lg-9 text-black prod__detail">
     <h2>{{$project->title}}</h2>
     <p><strong>Starting Date:</strong>{{date('j F, Y', $project->start_date)}}<br>
@@ -14,5 +15,5 @@
 ?>
 
  </div>
-
+</div>
 @endsection
