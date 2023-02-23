2a39f93000e8e5db97d1fd1b8275bf7a36c0a68d
sagardhakal <sagar.dhakal312@gmail.com>
2023-02-23 22:37:40 +0545

    drawer collapse

--- config/filament.php
+++ config/filament.php
@@ -139,8 +139,7 @@ return [
         'namespace' => 'App\\Filament\\Widgets',
         'path' => app_path('Filament/Widgets'),
         'register' => [
-            Widgets\AccountWidget::class,
-            Widgets\FilamentInfoWidget::class,
+            Widgets\AccountWidget::class
         ],
     ],
 
@@ -244,7 +243,7 @@ return [
             'alignment' => 'right',
         ],
         'sidebar' => [
-            'is_collapsible_on_desktop' => false,
+            'is_collapsible_on_desktop' => true,
             'groups' => [
                 'are_collapsible' => true,
             ],
