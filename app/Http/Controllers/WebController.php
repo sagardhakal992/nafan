4ab8dbd1b8aa41c78045820458a2dc770f6c4ee0
sagardhakal <sagar.dhakal312@gmail.com>
2023-02-23 10:27:17 +0545

    filament added

--- app/Http/Controllers/WebController.php
+++ app/Http/Controllers/WebController.php
@@ -13,7 +13,7 @@ class WebController extends Controller
     public function home(){
         $currentProjects = Project::where('is_completed', false)->orderByDesc("created_at")->take(6)->get();
         $pastProjects = Project::where('is_completed', true)->orderByDesc("created_at")->take(6)->get();
-        $posts = Post::take(6)->get();
+        $posts = Post::where("is_public",1)->take(6);
         return view('welcome',["currentProjects"=>$currentProjects,"posts"=>$posts,"pastProjects"=>$pastProjects]);
     }
 
