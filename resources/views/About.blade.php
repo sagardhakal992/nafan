77c3d01f739f639719f3b8fadb3d4aee24e0ad6a
sagardhakal <sagar.dhakal312@gmail.com>
2023-05-06 15:42:42 +0545

    changed content

--- resources/views/About.blade.php
+++ resources/views/About.blade.php
@@ -4,6 +4,10 @@
 <div class="container">
     <h2>About Us</h2>
     <div>
+        <h3>Information</h3>
+        <p>National forum for Advocacy Nepal (NAFAN) is a social organization which was formally established in 2004 (2063BS) by the professional, academician, expert, campaigner who have keen interest to undertake advocacy and carry out development initiatives in Nepal/activities in different fields on behalf of Nepal’s civil society. NAFAN has been working on various thematic issues such as environment, sustainable development human right, peace building and other cross cutting issues. This organization has been prioritizing the issues of peoples right on resources by effective, efficient, and managed advocacy procedure for the improvement of livelihood of backwarded, poor and marginalized community. NAFAN has been working in the field of environmental conservation, sustainable resource management and its equitable distribution and capacity building, promoting gender equity and equality. Currently, NAFAN is partnering with Swallows Finland, Rydam Foundation, Raksirang Gaunpalika and other organizations for various initiatives in Nepal.</p>
+    </div>
+    <div>
         <h3>1. Background</h3>
         <p style="font-size: 120%">The Regional Planning Workshop on Capacity Building of Community Based Organizations in Advocacy Strategies organized by the International Center for Integrated Mountain Development (ICIMOD) in Chittagong city of Bangladesh from 3 to 6 November 2003 was the founding stone of NAFAN. The representatives of civil society organizations from Bangladesh, India, Nepal, and Pakistan were participating in this workshop. The workshop was facilitated by experts in advocacy strategies connected with the Center for Advocacy Strategies, Pune India.</p>
         <br />
@@ -20,31 +24,60 @@
     <div>
         <h3>3.Vision, mission, and objectives</h3>
         <h3>Vision</h3>
-        <p style="font-size: 120%">NAFAN likes to achieve improved living standards for the Nepalese people, marginalized and disadvantaged groups in particular, through action-oriented activities and public advocacy based on the needs and interests of working communities.</p>
+        <p style="font-size: 120%">Peace and prosperity of the nation and environmental conservation.</p>
+        {{-- <p style="font-size: 120%">NAFAN likes to achieve improved living standards for the Nepalese people, marginalized and disadvantaged groups in particular, through action-oriented activities and public advocacy based on the needs and interests of working communities.</p> --}}
         <h3>Mission</h3>
 
-        <p style="font-size: 120%">To support members, partners, and other like-minded organizations on pilot projects and advocacy initiatives by establishing a well-equipped resource and a trained team at NAFAN.</p>
+        {{-- <p style="font-size: 120%">To support members, partners, and other like-minded organizations on pilot projects and advocacy initiatives by establishing a well-equipped resource and a trained team at NAFAN.</p> --}}
+        <p style="font-size: 120%">Influence, encourage and shape the plan, policies and decisions in political, social, economic and environmental dimension</p>
         <h3>Objective</h3>
         <ul style="margin-left:15px;list-style:square"">
-            <li style="margin-top: 5px;font-size:120%">	Promote Human Rights in socially and economically marginalized communities.</li>
-            <li style="margin-top: 5px;font-size:120%">	Promote cultural and economic rights of different classes and communities.</li>
-            <li style="margin-top: 5px;font-size:120%">	Enhance access of marginalized classes to the environment, natural resources, climate change, and biodiversity.</li>
-            <li style="margin-top: 5px;font-size:120%">	Provide a proportionate share to the disadvantaged communities, and local groups of the benefits arising from commercial and other utilization of natural or biological resources.</li>
-            <li style="margin-top: 5px;font-size:120%">	Carry out different activities to ensure the rights of Women mainstreaming Gender.</li>
-            <li style="margin-top: 5px;font-size:120%">Support decentralizing natural resource management, conservation, sustainable use, and management of biodiversity to the lowest level of society and local government.</li>
-            <li style="margin-top: 5px;font-size:120%">	Establish and promote good environmental governance.</li>
-            <li style="margin-top: 5px;font-size:120%">	Ensure the rights of forest workers' as per the ILO conventions and provisions.</li>
-            <li style="margin-top: 5px;font-size:120%">	Act as a Resource Centre in advocacy strategies, and</li>
+            <li style="margin-top: 5px;font-size:120%">	Protection, promotion, and realization of human rights of socially and economically marginalized people and community</li>
+            <li style="margin-top: 5px;font-size:120%">	Protection, promotion and realization of the cultural and economic right of different class and community</li>
+            <li style="margin-top: 5px;font-size:120%">	Access of marginalized class to environment, natural resources and biodiversity</li>
+            <li style="margin-top: 5px;font-size:120%">	Provide proportionate share to the disadvantaged community, and local groups of the benefits arising from commercial and other utilization of natural or biological resources</li>
+            <li style="margin-top: 5px;font-size:120%">	Decentralization of natural resource management, conservation, sustainable use it and management of biodiversity to the local level of the society and local government</li>
+            <li style="margin-top: 5px;font-size:120%">Establish and promote good environmental governance</li>
+            <li style="margin-top: 5px;font-size:120%">	Act as a resource center</li>
+        </ul>
+    </div>
+    <div>
+        <h3>Organogram</h3>
+        <img src="{{asset('assets/frontend/img/organogram.jpg')}}" style="height:100%;width:100%"/>
+    </div>
+    <div>
+        <h3 >4. Act as a Resource Centre in advocacy strategies</h3>
+        <ul style="margin-left:25px;list-style:square">
+            <li style="margin-top: 5px;font-size:120%">	Reorganization gained at the national and regional levels.</li>
+            <li style="margin-top: 5px;font-size:120%">	Synergies created through collective efforts of members in the environment and gender justice.</li>
+            <li style="margin-top: 5px;font-size:120%">	Build up mutual trust and solidarity among members.</li>
+        </ul>
+    </div>
+    <div>
+        <h3>5. VALUES</h3>
+        <ul style="margin-left:25px;list-style:square">
+                <li style="margin-top: 5px;font-size:120%">Policy contribution on land, water, forest and climate change</li>
+                <li style="margin-top: 5px;font-size:120%">Huge publication on natural resource management</li>
+                <li style="margin-top: 5px;font-size:120%">Known as a policy advocacy organization</li>
+                <li style="margin-top: 5px;font-size:120%">Trusted by donor and program operation capacity</li>
+                <li style="margin-top: 5px;font-size:120%">Currently managing 15 staffs and secured fund for next 3  years</li>
+
         </ul>
     </div>
 
     <div>
-        <h3>4.Act as a Resource Centre in advocacy strategies, and</h3>
-        <ul style="margin-left:15px;list-style:square"">
-            <li style="margin-top: 5px;font-size:120%">	Reorganization gained at the national and regional levels.</li>
-            <li style="margin-top: 5px;font-size:120%">	Synergies created through collective efforts of members in the environment and gender justice.</li>
-            <li style="margin-top: 5px;font-size:120%">	Build up mutual trust and solidarity among members.</li>
-
+        <h3>6. MAJOR ACHIEVEMENTS of NAFAN</h3>
+        <ul style="margin-left:25px;list-style:square"">
+            <li style="margin-top: 5px;font-size:120%">Mutual respect and solidarity</li>
+            <li style="margin-top: 5px;font-size:120%">Participation and active involvement</li>
+            <li style="margin-top: 5px;font-size:120%">Equity, equality and inclusion</li>
+            <li style="margin-top: 5px;font-size:120%">Transparency and accountability</li>
+            <li style="margin-top: 5px;font-size:120%">Democracy and human rights</li>
+            <li style="margin-top: 5px;font-size:120%">Collaboration and Collective leadership</li>
+            <li style="margin-top: 5px;font-size:120%">Peaceful social transformation</li>
+            <li style="margin-top: 5px;font-size:120%">Decentralized decision</li>
+            <li style="margin-top: 5px;font-size:120%">Biasness</li>
+            <li style="margin-top: 5px;font-size:120%">Local resource mobilization</li>
         </ul>
     </div>
 </div>
--- resources/views/WorkingArea.blade.php
+++ resources/views/WorkingArea.blade.php
@@ -3,20 +3,107 @@
 <div class="container">
     <h2>Working Area</h2>
     <div>
-        <h3>1. Thematic area of NAFAN </h3>
+        <h3>1. Thematic Priorities and Defined Activities for 2022-2027  </h3>
         <ul style="margin-left:15px;list-style:square"">
-            <li style="margin-top: 5px;font-size:120%">	Conflict: Conflict management related to Natural Resource Management (NRM) including mediation.</li>
-            <li style="margin-top: 5px;font-size:120%">	Health: Hygiene, Sanitation, and Nutrition. </li>
-            <li style="margin-top: 5px;font-size:120%">	Forestry: Natural Resource management, REDD+, forestry and watershed management </li>
-            <li style="margin-top: 5px;font-size:120%">Climate Change: Climate Change and Disaster Risk Reduction (DRR)</li>
-            <li style="margin-top: 5px;font-size:120%">Capacity Building: Organization management, climate change, advocacy strategies, and natural resource management.</li>
-            <li style="margin-top: 5px;font-size:120%">Advocacy: Strategies, tools, and techniques.</li>
-            <li style="margin-top: 5px;font-size:120%">	Gender and Social Inclusion (GESI): Cross-cutting in all cts and activities.</li>
+            <li style="margin-top: 5px;font-size:120%">	Natural Resource Management
+            <p>Possibe donors: WWF, GEF, Practical Action, Sallow Foundation, </p>
+            </li>
+            <ol>
+                <li>Provide Entrepreneurship Development Training</li>
+                <li>Support Seed money for their entrepreneurship</li>
+                <li>Marketing</li>
+                <li>Value Chain Analysis</li>
+            </ol>
+            <li>
+                Resilience Livelihood
+               <p> Possible Donors:   <b>Sallow Foundation, DCA, UNDP </b></p>
+            </li>
+            <ol>
+              <li>Provide Entrepreneurship Development Training</li>
+              <li>Support Seed money for their entrepreneurship</li>
+              <li>Marketing</li>
+              <li>Value Chain Analysis</li>
+            </ol>
+            <li>Human Right and Good Governance and Accountability:<p>
+            Possible Donors:   <b>Human Right commission, UN agencies, Asia foundation, US Embassies, EU</b>
+            </p></li>
+            <ol>
+                <li>Interaction and Discussion</li>
+                <li>Public Hearing and Social Audit</li>
+                <li>Promote Human Rights and respect others dignity</li>
+                <li>Training and research</li>
+            </ol>
+            <li> DRR CCA
+            <p> Possible Donors: TAYAR, Ministry of Home, Various Organization </p>
+            </li>
+            <ol>
+                <li>Interaction and Discussion</li>
+                <li>Public Hearing and Social Audit</li>
+                <li>Promote Human Rights and respect others dignity</li>
+                <li>Training and research</li>
+            </ol>
+            <li>Policy Research and Advocacy
+                <p> Possible Donors: US embassies, EU, Koica, World Bank, </p>
+            </li>
+            <ol>
+                <li>Policy research on NRM, Human rights, Law on forest, land and water</li>
+                <li>Government lobby and Advocacy at central, regional and local government</li>
+                <li>Capacity building on Research and advocacy</li>
+            </ol>
         </ul>
     </div>
 
     <div>
-        <h3>2. Thematic area of NAFAN </h3>
+        <h3>2. Institution Development  </h3>
+        <ul style="margin-left:15px;list-style:square"">
+            <li style="margin-top: 5px;font-size:120%">Enhancing organizational capacity</li>
+            <ol>
+                <li>Expand membership in each year</li>
+                <li>develop Human Resource Development Unite</li>
+                <li>Develop Programme Develop Unit</li>
+                <li>Provide Baseline survey, Report Writing, Proposal Writing, M&E training</li>
+                <li>Well -Equipped furniture and Furnishing</li>
+            </ol>
+            <li>Resource mobilization and financial sustainability</li>
+            <ol>
+               <li> Local resource mobilization</li>
+               <li>best utilization of organizations expertise</li>
+               <li>Create organization core fund</li>
+            </ol>
+            <li>Create organization core fund</li>
+            <ol>
+                <li>Public Audit</li>
+                <li>Regular audit</li>
+            </ol>
+            <li>
+                Program collaboration and issue based networking
+            </li>
+            <ol>
+                <li>Network and coordination with other stakeholder and like minded organization for youth participation</li>
+                <li>Lobby for betterment of life and people dignity</li>
+            </ol>
+            <li>Coordination, communication and relationship building</li>
+            <ol>
+                <li>Regular communication with other stakeholders</li>
+                <li>Regular communication with Donor and Development aid organization</li>
+                <li>Communication tools can be Phone, Internet, Email as per suitability</li>
+            </ol>
+            <li>Enhancing organizational capacity</li>
+            <ol>
+               <li> HRD</li>
+                <li>Strengthen Administrative and Financial policy</li>
+                <li>Develop Communication Policy</li>
+                <li>Develop code of conduct for staffs and members</li>
+                <li>follow the best practice of organization</li>
+            </ol>
+        </ul>
+    </div>
+    <div>
+        <h3>3. 5 Years Of Visioning</h3>
+        <img src="{{asset('assets/frontend/img/5years.jpg')}}" style="height:100%;width:100%" />
+    </div>
+    <div style="padding:5px">
+        <h3>4. Thematic area of NAFAN </h3>
         <h3>Network organization</h3>
         <p style="font-size: 120%">NAFAN is an umbrella of organizations working on rights-based approaches and advocacy. Therefore, NAFAN has been sup0porting to any of the organizations based on their needs throughout Nepal.</p>
         <h3>Field level project</h3>
@@ -25,7 +112,7 @@
     </div>
 
     <div>
-        <h3>3. Funding partners of NAFAN </h3>
+        <h3>5. Funding partners of NAFAN </h3>
         <p style="font-size: 120%">ICIMOD was a founding partner of NAFAN. Later on, NAFAN worked with Word Wildlife Federation (WWF), ICCO, CECI, Federation of Community Forest Users in Nepal (FECOFUN), ACOFUN, DANAR. MSFP, GFC. BWI and the Government of Nepal with various Ministries. </p>
 
     </div>
