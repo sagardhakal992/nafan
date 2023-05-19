f7ad591f9bc97c4c847f10b48f7d51de02871015
sagardhakal <sagar.dhakal312@gmail.com>
2023-05-19 15:35:09 +0545

    post

--- app/Policies/PostPolicy.php
+++ app/Policies/PostPolicy.php
@@ -53,7 +53,7 @@ class PostPolicy
      */
     public function update(User $user, Post $post)
     {
-        return false;
+        return true;
     }
 
     /**
@@ -65,7 +65,7 @@ class PostPolicy
      */
     public function delete(User $user, Post $post)
     {
-        return false;
+        return true;
     }
 
     /**
