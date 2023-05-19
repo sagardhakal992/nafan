564261cebf8ce82964b4b239181d6bdf97793aa0
sagardhakal <sagar.dhakal312@gmail.com>
2023-05-19 17:48:04 +0545

    edit error

--- app/Filament/Resources/MemberResource/Pages/EditMember.php
+++ app/Filament/Resources/MemberResource/Pages/EditMember.php
@@ -3,12 +3,27 @@
 namespace App\Filament\Resources\MemberResource\Pages;
 
 use App\Filament\Resources\MemberResource;
+use App\Models\Member;
 use Filament\Pages\Actions;
 use Filament\Resources\Pages\EditRecord;
+use Illuminate\Database\Eloquent\Model;
 
 class EditMember extends EditRecord
 {
     protected static string $resource = MemberResource::class;
+
+    protected function handleRecordUpdate(Model $record, array $data): Model
+    {
+        $newData = [
+            "name" => $data["name"],
+            "email" => $data["email"],
+            "age" => $data["age"] ?? null,
+            "phone_number" => $data["phone_number"],
+            'fk_user_id' => $data['id']
+        ];
+        $record->update($newData);
+        return $record;
+    }
 
     protected function getActions(): array
     {
